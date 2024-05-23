15a59fa2e7e0921db63b9b59f5cf4134236dd3b0
ZhuLei <lei.zhu@reach-health.com.cn>
2024-05-23 13:07:56 +0800

    Initial commit

---
+++ src/main/java/com/juanwang/aftermodel/AfterModelApplication.java
@@ -0,0 +1,13 @@
+package com.juanwang.aftermodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfterModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AfterModelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/juanwang/aftermodel/AfterModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanwang.aftermodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfterModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
